9c4aafefafdf7e8b2f3a918b325e8f0d35600a4a
David Christ <dch_dev@genitopia.org>
2014-02-23 10:28:34 +0100

    Move to constants

--- sapful.php
+++ sapful.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_FILES['sapfulUploadFiles'])) {
 	// todo: verify file name for bad chars
-	move_uploaded_file($_FILES['sapfulUploadFiles']['tmp_name'], '/var/local/lib/sapful/' . basename($_FILES['sapfulUploadFiles']['name']));
-	
+	move_uploaded_file($_FILES['sapfulUploadFiles']['tmp_name'], DESTDIR . basename($_FILES['sapfulUploadFiles']['name']));
+
 	// If the post field exist, the form was submited directly (not with AJAX) and a redirect to page should be done.
 	if (isset($_POST['submit'])) {
 		$status = 'Done';
